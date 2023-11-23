a80af048fd3700a15da8277aeb08a544070fddf2
hamzashahid <hamzashahid256@gmail.com>
2023-11-24 02:28:25 +0500

    Create basic structure of models

---
+++ myapp/models.py
@@ -0,0 +1,125 @@
+from django.db import models
+from django.urls import reverse
+
+from django.contrib.auth.models import User
+from django.core.validators import FileExtensionValidator
+
+ALLOWED_EXTENSIONS = ['png', 'jpg', 'jpeg']
+
+
+class UserProfile(models.Model):
+    title = models.CharField(max_length=255, null=True, blank=True)
+    phone_number = models.CharField(max_length=12, null=True, blank=True)
+    address = models.CharField(max_length=255, null=True, blank=True)
+    image = models.FileField(upload_to='static/images', null=True, blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user
+
+
+class Service(models.Model):
+    title = models.CharField(max_length=150, blank=False, unique=True)
+    description = models.CharField(max_length=512, blank=False, unique=True)
+    icon = models.FileField(upload_to='static/images', validators=[
+        FileExtensionValidator(allowed_extensions=ALLOWED_EXTENSIONS)], default="")
+
+
+class ContactForm(models.Model):
+    name = models.CharField(max_length=20, help_text='required')
+    email = models.EmailField(max_length=100, help_text='required')
+    subject = models.CharField(max_length=30)
+    message = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.email
+
+
+class feedback(models.Model):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=30, help_text='required')
+    rating = models.CharField(max_length=10)
+    message = models.CharField(max_length=512)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.username
+
+
+class CompanyLogo(models.Model):
+    logo = models.FileField(upload_to='static/images', validators=[
+        FileExtensionValidator(allowed_extensions=['png', 'jpg', 'jpeg'])],
+                            default="")
+
+
+class Banner(models.Model):
+    title = models.CharField(max_length=150, blank=False, unique=True)
+    description = models.CharField(max_length=512, blank=False, unique=True)
+    icon = models.FileField(upload_to='static/images', validators=[
+        FileExtensionValidator(allowed_extensions=ALLOWED_EXTENSIONS)], default="")
+
+
+class Technology(models.Model):
+    title = models.CharField(max_length=150, blank=False, unique=True)
+    icon = models.FileField(upload_to='static/images', validators=[
+        FileExtensionValidator(allowed_extensions=ALLOWED_EXTENSIONS)], default="")
+
+
+class Template(models.Model):
+    title = models.CharField(max_length=150, blank=False, unique=True)
+    image = models.FileField(upload_to='static/images', validators=[
+        FileExtensionValidator(allowed_extensions=ALLOWED_EXTENSIONS)], default="")
+    link = models.URLField(max_length=255, default=None)
+    price = models.FloatField(null=True, blank=True)
+
+
+class Notice(models.Model):
+    title = models.CharField(max_length=150, blank=False, unique=True)
+    date = models.DateTimeField(null=True, blank=True)
+    image = models.FileField(upload_to='static/images', default="")
+
+
+class Order(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    order_id = models.CharField(max_length=20, null=True, blank=True)
+    items = models.JSONField(default=dict)
+    total = models.FloatField(default=None, blank=True, null=True)
+    created_at = models.DateField(auto_now_add=True)
+    is_approve = models.BooleanField(default=False)
+    first_name = models.CharField(max_length=30, null=True, blank=True, default=None)
+    last_name = models.CharField(max_length=30, default=None)
+    email = models.EmailField(blank=True)
+    zip_code = models.CharField(max_length=10, null=True, blank=True)
+    phone_number = models.CharField(max_length=13, blank=False, default=None)
+    address = models.CharField(max_length=150, default=" ")
+    delivery_instruction = models.CharField(max_length=250, blank=True, null=True)
+    deliver_date = models.DateField(null=True, blank=True)
+    is_deliver = models.BooleanField(default=False, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
+    def __str__(self) -> str:
+        return self.order_id
+
+    @classmethod
+    def send_mail(cls, sender, context):
+        pass
+
+
+class GlobalLocation(models.Model):
+    email = models.EmailField(max_length=255, null=True)
+    phone_number = models.CharField(max_length=15, null=True, blank=True)
+    country = models.CharField(max_length=255, null=True, blank=True)
+    address = models.CharField(max_length=255, null=True, blank=True)
+
+
+class SocialMedia(models.Model):
+    icon = models.FileField(upload_to='static/images', validators=[
+        FileExtensionValidator(allowed_extensions=ALLOWED_EXTENSIONS)], default="")
+    link = models.URLField(max_length=255, null=True, blank=True)
+
+
+def get_or_none(model, **kwargs):
+    try:
+        return model.objects.get(**kwargs)
+    except model.DoesNotExist:
+        return None
---
+++ myapp/views.py
@@ -0,0 +1,65 @@
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import generics, status
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+
+from django.contrib.auth import login
+
+# Token Authentication
+from knox.models import AuthToken
+from knox.views import LoginView as KnoxLoginView
+from knox.auth import TokenAuthentication
+
+from .models import get_or_none
+from .serializers import UserSerializer, RegisterSerializer, VerifyUserSerializer
+
+from django.contrib.auth.models import User
+
+
+class UserDetailAPI(APIView):
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request, *args, **kwargs):
+        user = request.user
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+
+# Class based view to register user
+class RegisterUserAPIView(generics.CreateAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response({
+            "user": UserSerializer(user, context=self.get_serializer_context()).data,
+            "token": AuthToken.objects.create(user)[1]
+        })
+
+
+class LoginAPI(KnoxLoginView):
+    permission_classes = (AllowAny,)
+
+    def post(self, request, format=None):
+        serializer = AuthTokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        login(request, user)
+        return super(LoginAPI, self).post(request, format=None)
+
+
+class UserVerify(APIView):
+
+    def get(self, request):
+        serializer = VerifyUserSerializer(data=request.query_params)
+        serializer.is_valid(raise_exception=True)
+        user = get_or_none(User, **serializer.validated_data)
+        if user:
+            return Response({'msg': 'ok'}, status=200)
+
+        return Response({'msg': 'No user associated with this email or username!'}, status=200)
